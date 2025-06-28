4bdbcaf88c5a0bcb33cebd4c2cb19d2907fc1d6a
Neo <99268950+n30w@users.noreply.github.com>
2025-06-28 07:19:26 -1000

    chore(treefmt): use `pkgs` packages

--- treefmt.nix
+++ treefmt.nix
@@ -2,11 +2,10 @@
 #
 # SPDX-License-Identifier: CC0-1.0
 
+{ pkgs, ... }:
 {
   # See https://github.com/numtide/treefmt-nix#supported-programs
-
-  projectRootFile = "./flake.nix";
-
+  projectRootFile = ".git/config";
   settings.global.includes = [
     "*.go"
     ".toml"
@@ -18,31 +17,33 @@
     "*.sql"
     "*.sh"
   ];
-
   # settings.global.fail-on-change = false;
   # settings.global.no-cache = true;
-
-  programs.gofumpt.enable = true;
+  programs.gofumpt = {
+    enable = true;
+    package = pkgs.gofumpt;
+  };
   programs.goimports.enable = true;
   programs.golines.enable = true;
-  programs.buf.enable = true; # protobuf
+  programs.buf = {
+    enable = true;
+    package = pkgs.buf;
+  };
   programs.sql-formatter = {
     enable = true;
     dialect = "postgresql";
   };
-
   # GitHub Actions
   programs.yamlfmt.enable = true;
   programs.actionlint.enable = true;
-
   programs.taplo.enable = true;
-
   # Markdown
   programs.mdformat.enable = true;
-
   # Nix
-  programs.nixfmt.enable = true;
-
+  programs.nixfmt = {
+    enable = true;
+    package = pkgs.nixfmt-rfc-style;
+  };
   # Shell
   programs.shfmt.enable = true;
 }
